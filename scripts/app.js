014f9536c5205f5dcc9f9a97ba3624f2278db792
lekan <kokulekan23@gmail.com>
2019-04-18 19:24:33 +0200

    added new animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 function openNav() {
     document.getElementById("nav").style.width = "100%";
+    // console.log(document.getElementsByClassName("overlayLink"));
+
+    var links = document.getElementsByClassName("overlayLink");
+    for (let element of links) {
+        element.style.animation = "slideInBottom 0.3s";
+    }
 }
 function closeNav() {
     document.getElementById("nav").style.width = "0";
+    var links = document.getElementsByClassName("overlayLink");
+    for (let element of links) {
+        element.style.animation = "";
+    }
 }
