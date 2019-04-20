817331fbbed3aae60c2eebace2db0722d0bf8f5e
lekan <kokulekan23@gmail.com>
2019-04-20 09:21:00 +0200

    added SASS

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 function openNav() {
     document.getElementById("nav").style.width = "100%";
-
     var links = document.getElementsByClassName("overlayLink");
     for (let element of links) {
         element.style.animation = "slideInBottom 0.3s";
